aa11d6a53158c44996c17f0cf3667d7082538595
GABRIEL HENRIQUE COSTA DA SILVA <gabriel.07907@aluno.pr.senac.br>
2023-09-05 15:44:59 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/exemplorepositorio/ExemploRepositorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplorepositorio;
+
+/**
+ *
+ * @author gabriel.07907
+ */
+public class ExemploRepositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
