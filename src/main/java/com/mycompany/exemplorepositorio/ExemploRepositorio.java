882a75e19e9db68005747d15d98cc77640a77129
GABRIEL HENRIQUE COSTA DA SILVA <gabriel.07907@aluno.pr.senac.br>
2023-09-05 16:23:07 -0300

    primeira alteração

--- src/main/java/com/mycompany/exemplorepositorio/ExemploRepositorio.java
+++ src/main/java/com/mycompany/exemplorepositorio/ExemploRepositorio.java
@@ -11,6 +11,6 @@ package com.mycompany.exemplorepositorio;
 public class ExemploRepositorio {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("está alterado");
     }
 }
